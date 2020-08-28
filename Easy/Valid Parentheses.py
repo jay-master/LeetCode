67b9ea95698e7914abdf52f7275cac4b15acdd12
jay-master <57156119+jay-master@users.noreply.github.com>
2020-08-28 17:41:17 +0200

    Update Valid Parentheses.py

--- Easy/Valid Parentheses.py
+++ Easy/Valid Parentheses.py
@@ -32,8 +32,8 @@ Output: true
 
 
 Submission Result:
-Runtime: 2660 ms, faster than 6.29% of Python online submissions for Valid Parentheses.
-Memory Usage: 13 MB, less than 17.66% of Python online submissions for Valid Parentheses.
+Runtime: Runtime: 20 ms, faster than 81.04% of Python online submissions for Valid Parentheses.
+Memory Usage: 12.9 MB, less than 30.01% of Python online submissions for Valid Parentheses.
 """
 
 class Solution (object):
@@ -50,21 +50,26 @@ class Solution (object):
 
         for key in pair:
             if key in s:    # For every 'opening' parentheses in input 's', check if i+1, i+3, i+5, ..-th elements are their 'pair' parentheses (here i: index of 'opening' parentheses).
-                i = 1
-                j = 0   # Reset for the next element
-                while i + s.index(key) < len(s):
-                    if s[s.index(key) + i] == pair[key]:
-                        j += 1  # Increase variable 'j', if there are 'pair' of i-th element in i+1, i+3, i+5, ..-th position.
-                        i += 2
-                    else:   # Do not increase variable 'j', if there is no 'pair' of i-th element in i+1, i+3, i+5, ..-th position.
-                        i += 2
-                checkpair.append(j) # After check i-th element (and i+1, i+3, i+5, ..-th elements), put variable 'j' in the list. If 'j' is 0, i-th element has no 'pair' in proper positions.
+                if s.count(key) == s.count(pair[key]):  # Put a filtering condition at front part, instead back part. If input doesn't meet condition, the result returned immediately.
+                    i = 1
+                    j = 0   # Reset for the next element
+                    while i + s.index(key) < len(s):
+                        if s[s.index(key) + i] == pair[key]:
+                            j += 1  # Increase variable 'j', if there are 'pair' of i-th element in i+1, i+3, i+5, ..-th position.
+                            i += 2
+                        else:   # Do not increase variable 'j', if there is no 'pair' of i-th element in i+1, i+3, i+5, ..-th position.
+                            i += 2
+                    checkpair.append(j) # After check i-th element (and i+1, i+3, i+5, ..-th elements), put variable 'j' in the list. If 'j' is 0, i-th element has no 'pair' in proper positions.
+                else:
+                    return False    # If input doesn't meet condition, the result returned immediately.
+            else:
+                if s.count(pair[key]) != 0:
+                    return False    # Return False, if any 'closing' parentheses exists, even though there is no 'opening' pair-parentheses.
 
         print(checkpair)
 
-        # Many filtering conditions are written here.
-        if 0 not in checkpair and checkpair != [] and s.count('(') == s.count(')') and s.count('[') == s.count(
-                ']') and s.count('{') == s.count('}') or s == "":
+        # Less filtering conditions are written here.
+        if 0 not in checkpair and checkpair != [] or s == "":
             return True
         else:
             return False
@@ -74,6 +79,6 @@ ValPar = Solution()
 print(ValPar.isValid(s))
 
 """
-In the end this results in inefficient computation as the filtering is situated at the end, i.e., this code can not filter the input from the beginning.
-If it can filter the input from the beginning, the rest of the computation is not required, i.e., more efficient.
+This code can filter the input from the beginning.
+As a result, unnecessary further computation can be avoided.
 """
